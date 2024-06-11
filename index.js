aa6f428105772da8ebdc1da52838bba06376ee15
David M <62346025+aboutdavid@users.noreply.github.com>
2024-06-11 17:22:38 -0400

    add prettier ci + fmt

--- index.js
+++ index.js
@@ -4,107 +4,105 @@ const fs = require("fs");
 const { createClient } = require("redis");
 const pms = require("pretty-ms");
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: !Boolean(process.env.PORT),
-    appToken: process.env.SLACK_APP_TOKEN,
-    port: process.env.PORT
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: !Boolean(process.env.PORT),
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT,
 });
 
-
-
-
 (async () => {
+  const client = await createClient({
+    url: process.env.REDIS_DATABASE,
+  })
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
 
-    const client = await createClient({
-        url: process.env.REDIS_DATABASE
-    })
-        .on('error', err => console.log('Redis Client Error', err))
-        .connect();
-
-    async function redoMessage() {
-
-        const data = await app.client.conversations.history({
-            channel: process.env.SLACK_CHANNEL,
-        });
-        const { messages } = data;
-
-        await Promise.all(
-            messages.map((message) =>
-                app.client.chat
-                    .delete({
-                        token: process.env.SLACK_USER_TOKEN,
-                        channel: process.env.SLACK_CHANNEL,
-                        ts: message?.ts,
-                        thread_ts: message?.thread_ts,
-                    })
-                    .catch((e) => {
-                        console.warn(e);
-                    }),
-            ),
-        );
-        const tmesg = await app.client.chat.postMessage({
-            channel: process.env.SLACK_CHANNEL,
-            text: "Loading drectory",
-        });
-        await client.set("messageId", tmesg.ts)
-    }
-
-    setInterval(redoMessage, 1000 * 60 * 60 * 12)
-    await redoMessage()
-    await app.start(process.env.PORT || 3000);
-
-    app.message(/.*/gim, async ({ message, say }) => {
-
-        //if (await client.exists("newChannelMessage") && parseInt(client.get("newChannelMessage")) > Date.now()) return
-        if (await client.exists("messageText") && await client.exists("messageId")) {
-            const tmpText = await client.get("messageText")
-            const newText = tmpText.replace(/Latest message: .*? ago/, `Latest message: ${pms(Date.now() - Math.floor(parseInt(message.ts) * 1000))} ago`)
-            console.log(await client.get("messageId"), process.env.SLACK_CHANNEL)
-            app.client.chat.update({
-                channel: process.env.SLACK_CHANNEL,
-                ts: await client.get("messageId"),
-                text: newText
-            })
-        }
-
+  async function redoMessage() {
+    const data = await app.client.conversations.history({
+      channel: process.env.SLACK_CHANNEL,
     });
-    var text = ""
+    const { messages } = data;
 
-    async function pull() {
-        text = ""
-        let sPromises = fs
-            .readdirSync("./sections")
-            .filter((str) => str.endsWith(".js"))
-            .sort()
-            .map(async (fn) => {
-                const section = await require(`./sections/${fn}`);
-                const rend = (await section.render({ app })).trim();
-                text += `${section.title}\n\n${rend}\n\n════════════════════════════════════\n`
-
-            });
-
-        await Promise.all(sPromises);
-        /*
-             */
-        text += `\nLast Updated on ${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeStyle: "short", dateStyle: "long" })} (EST)`
-        client.set("messageText", text)
-        app.client.chat.update({
+    await Promise.all(
+      messages.map((message) =>
+        app.client.chat
+          .delete({
+            token: process.env.SLACK_USER_TOKEN,
             channel: process.env.SLACK_CHANNEL,
-            ts: await client.get("messageId"),
-            text,
-        });
-    }
-    await pull();
-    setInterval(pull, 1000 * 30);
+            ts: message?.ts,
+            thread_ts: message?.thread_ts,
+          })
+          .catch((e) => {
+            console.warn(e);
+          }),
+      ),
+    );
+    const tmesg = await app.client.chat.postMessage({
+      channel: process.env.SLACK_CHANNEL,
+      text: "Loading drectory",
+    });
+    await client.set("messageId", tmesg.ts);
+  }
 
-    app.message(/.*/gim, async ({ message }) => {
-        if (message.channel == process.env.SLACK_CHANNEL)
-            await app.client.chat.delete({
-                channel: message.channel,
-                ts: message.ts,
-                token: process.env.SLACK_USER_TOKEN
-            })
-    })
-    console.log("⚡️ Bolt app is running!");
+  setInterval(redoMessage, 1000 * 60 * 60 * 12);
+  await redoMessage();
+  await app.start(process.env.PORT || 3000);
+
+  app.message(/.*/gim, async ({ message, say }) => {
+    //if (await client.exists("newChannelMessage") && parseInt(client.get("newChannelMessage")) > Date.now()) return
+    if (
+      (await client.exists("messageText")) &&
+      (await client.exists("messageId"))
+    ) {
+      const tmpText = await client.get("messageText");
+      const newText = tmpText.replace(
+        /Latest message: .*? ago/,
+        `Latest message: ${pms(Date.now() - Math.floor(parseInt(message.ts) * 1000))} ago`,
+      );
+      console.log(await client.get("messageId"), process.env.SLACK_CHANNEL);
+      app.client.chat.update({
+        channel: process.env.SLACK_CHANNEL,
+        ts: await client.get("messageId"),
+        text: newText,
+      });
+    }
+  });
+  var text = "";
+
+  async function pull() {
+    text = "";
+    let sPromises = fs
+      .readdirSync("./sections")
+      .filter((str) => str.endsWith(".js"))
+      .sort()
+      .map(async (fn) => {
+        const section = await require(`./sections/${fn}`);
+        const rend = (await section.render({ app })).trim();
+        text += `${section.title}\n\n${rend}\n\n════════════════════════════════════\n`;
+      });
+
+    await Promise.all(sPromises);
+    /*
+     */
+    text += `\nLast Updated on ${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeStyle: "short", dateStyle: "long" })} (EST)`;
+    client.set("messageText", text);
+    app.client.chat.update({
+      channel: process.env.SLACK_CHANNEL,
+      ts: await client.get("messageId"),
+      text,
+    });
+  }
+  await pull();
+  setInterval(pull, 1000 * 30);
+
+  app.message(/.*/gim, async ({ message }) => {
+    if (message.channel == process.env.SLACK_CHANNEL)
+      await app.client.chat.delete({
+        channel: message.channel,
+        ts: message.ts,
+        token: process.env.SLACK_USER_TOKEN,
+      });
+  });
+  console.log("⚡️ Bolt app is running!");
 })();
