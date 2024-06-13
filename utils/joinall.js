c715e2c5255492e0379fe2083e696d4134102c9e
David M <62346025+aboutdavid@users.noreply.github.com>
2024-06-13 13:44:39 -0400

    Format

--- utils/joinall.js
+++ utils/joinall.js
@@ -2,34 +2,32 @@
  * @param {{app: import('@slack/bolt').App}} param1
  */
 module.exports = async function ({ app, client }) {
-    async function rake(cursor) {
-        if (process.env.JOIN_ALL_CHANNELS !== "abc") return
-        const convos = await app.client.conversations.list({
-            limit: 999,
-            cursor
-        })
-        let joinPromises = convos.channels.map(channel => {
-            return new Promise(resolve => {
-                setTimeout(async () => {
-                    await app.client.conversations.join({
-                        channel: channel.id,
-                    });
-                    await app.client.chat.postMessage({
-                        channel: channel.id,
-                        text: `Nice channel you got there. I'm librarian, which is created by HQ to help people find new and active channels. No message data is collected/stored, just how many messages are sent in a certain timeframe. If you do not want me in this channel and you do not want your channel in #directory, please run the command /optout-directory.`,
-                      });
-                    resolve();
-                }, 1000);
-            });
-        });
+  async function rake(cursor) {
+    if (process.env.JOIN_ALL_CHANNELS !== "abc") return;
+    const convos = await app.client.conversations.list({
+      limit: 999,
+      cursor,
+    });
+    let joinPromises = convos.channels.map((channel) => {
+      return new Promise((resolve) => {
+        setTimeout(async () => {
+          await app.client.conversations.join({
+            channel: channel.id,
+          });
+          await app.client.chat.postMessage({
+            channel: channel.id,
+            text: `Nice channel you got there. I'm librarian, which is created by HQ to help people find new and active channels. No message data is collected/stored, just how many messages are sent in a certain timeframe. If you do not want me in this channel and you do not want your channel in #directory, please run the command /optout-directory.`,
+          });
+          resolve();
+        }, 1000);
+      });
+    });
 
-        await Promise.all(joinPromises);
-        if (convos.response_metadata.next_cursor)
-            setTimeout(async function () {
-                await rake(convos.response_metadata.next_cursor)
-            }, 3000)
-        else
-            console.log("Finished joining all channels")
-
-    }
-}
+    await Promise.all(joinPromises);
+    if (convos.response_metadata.next_cursor)
+      setTimeout(async function () {
+        await rake(convos.response_metadata.next_cursor);
+      }, 3000);
+    else console.log("Finished joining all channels");
+  }
+};
--- utils/pull.js
+++ utils/pull.js
@@ -5,24 +5,24 @@ const fs = require("node:fs");
  * @param {{app: import('@slack/bolt').App}} param1
  */
 module.exports = async function ({ app, client }) {
-    var text = "";
-    let sPromises = fs
-        .readdirSync("./sections")
-        .filter((str) => str.endsWith(".js"))
-        .sort()
-        .map(async (fn) => {
-            const section = await require(`../sections/${fn}`);
-            const rend = (await section.render({ app })).trim();
-            text += `${section.title}\n\n${rend}\n\n════════════════════════════════════\n`;
-        });
-
-    await Promise.all(sPromises);
-
-    text += `\nLast Updated on ${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeStyle: "short", dateStyle: "long" })} (EST)`;
-    client.set("messageText", text);
-    app.client.chat.update({
-        channel: process.env.SLACK_CHANNEL,
-        ts: await client.get("messageId"),
-        text,
+  var text = "";
+  let sPromises = fs
+    .readdirSync("./sections")
+    .filter((str) => str.endsWith(".js"))
+    .sort()
+    .map(async (fn) => {
+      const section = await require(`../sections/${fn}`);
+      const rend = (await section.render({ app })).trim();
+      text += `${section.title}\n\n${rend}\n\n════════════════════════════════════\n`;
     });
+
+  await Promise.all(sPromises);
+
+  text += `\nLast Updated on ${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeStyle: "short", dateStyle: "long" })} (EST)`;
+  client.set("messageText", text);
+  app.client.chat.update({
+    channel: process.env.SLACK_CHANNEL,
+    ts: await client.get("messageId"),
+    text,
+  });
 };
