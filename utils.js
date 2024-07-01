808f6cb414112520e797d703dedc8d9340898d9c
David M <62346025+aboutdavid@users.noreply.github.com>
2024-07-01 09:42:04 -0400

    update blacklist

--- utils.js
+++ utils.js
@@ -1,20 +1,20 @@
 module.exports = {
   blockedChannels: [
     "C05G25Y9M6V", // #infra-alerts
-    "CLU651WHY", // #orpheus-internals
-    "CNMU9L92Q", // #confessions (reasons are obvious)
-    "CCZ5LRL6P", // #politics (reasons are obvious)
+    "CLU651WHY",   // #orpheus-internals
+    "CNMU9L92Q",   // #confessions (reasons are obvious)
+    "CCZ5LRL6P",   // #politics (reasons are obvious)
     "C0159TSJVH8", // #what-is-my-slack-id
     "C077R6LC3C1", // #diredctory
-    "CT0BV6UMV", //  #confessions-meta,
+    "CT0BV6UMV",   // #confessions-meta,
     "C0188CY57PZ", // #meta
     "C01T5J557AA", // #ssh-chat-bridge (banned users use this)
     "C02GK2TVAVB", // #temp-email
     "C078Q8PBD4G", // #library
     "C029E8FARRC", // #minecraft-bridge
 
-    // Here's a list of bot channels/spammy channels
-    // This goes here in order to prevent the indexing of artificially inflated rankings
+                   // Here's a list of bot channels/spammy channels
+                   // This goes here in order to prevent the indexing of artificially inflated rankings
     "C0P5NE354",
     "C06QL7WMRLK",
     "C02HSS9Q3D5",
@@ -28,6 +28,9 @@ module.exports = {
     "C02HSS9Q3D5",
     "C02B7CWDD0E",
     "C016DEDUL87",
+    "C070Y7F0PDG",
+    "C06SBHMQU8G", // arcade
+    "C077TSWKER0"  // arcade
   ],
   queries: {
     topThreads: `-is:dm is:thread -in:#arcade -in:#arcade-help -in:#arcade-lounge -in:#confessions -in:#meta -in:#scrapbook -in:#hackclub-leeks`,
