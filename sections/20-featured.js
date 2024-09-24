ff274bee1d3c2fba9dd25464ea1bbbcb6c1717f7
David M <62346025+aboutdavid@users.noreply.github.com>
2024-09-24 17:50:32 -0400

    add counterspell

--- sections/20-featured.js
+++ sections/20-featured.js
@@ -5,8 +5,10 @@ module.exports = {
    * @param {{app: import('@slack/bolt').App}} param1
    */
   render: async function ({ app }) {
-    return (text = `<#C75M7C0SY> - Introduce yourself to the community here
-<#C0266FRGV> - Meet new people here
+    return (text = `
+<#C07NWJSQR5J> - Hack Club's satelite game jam in over 200+ cities.
+<#C75M7C0SY> - Introduce yourself to the community here.
+<#C0266FRGV> - Meet new people here.
 <#C056WDR3MQR> - Free, powerful, and versatile compute infrastructure for all high school hackers!
 <#C01504DCLVD> - Share awesome updates about your work-in-progess projects`);
   },
