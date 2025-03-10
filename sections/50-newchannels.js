9e4a1dcd5d0c1290a70f1876f6d98c98dd307c07
David M <62346025+dispherical@users.noreply.github.com>
2025-03-09 22:43:04 -0400

    add new channels #library section

---
+++ sections/50-newchannels.js
@@ -0,0 +1,23 @@
+const utils = require("../utils");
+const { createClient } = require("redis");
+
+module.exports = {
+    title: ":public-channel: Recently created channels",
+    id: "newchannels",
+    description: "Here's a list of recently created channels",
+    /**
+     * @param {{app: import('@slack/bolt').App, prisma: import('@prisma/client').PrismaClient}} param1
+     */
+    render: async function ({ app, client, prisma }) {
+
+        const channels = await prisma.channel.findMany({
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+        const c = channels.slice(0, 10).map(channel => `- <#${channel.id}>`).join("\n")
+        return `Here's a list of recently created channels:\n${c}`
+            .replaceAll("@", "​@")
+            .replaceAll(/[\u{1F3FB}-\u{1F3FF}]/gmu, "");
+    },
+};
