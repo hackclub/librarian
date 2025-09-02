509c52b21b96d875bfece6b5f6bacfe4ee454de1
dispherical <62346025+dispherical@users.noreply.github.com>
2025-09-02 02:10:09 -0400

    fix: hide null channel names

--- sections/50-newchannels.js
+++ sections/50-newchannels.js
@@ -2,22 +2,22 @@ const utils = require("../utils");
 const { createClient } = require("redis");
 
 module.exports = {
-    title: ":public-channel: Recently created channels",
-    id: "newchannels",
-    description: "Here's a list of recently created channels",
-    /**
-     * @param {{app: import('@slack/bolt').App, prisma: import('@prisma/client').PrismaClient}} param1
-     */
-    render: async function ({ app, client, prisma }) {
+  title: ":public-channel: Recently created channels",
+  id: "newchannels",
+  description: "Here's a list of recently created channels",
+  /**
+   * @param {{app: import('@slack/bolt').App, prisma: import('@prisma/client').PrismaClient}} param1
+   */
+  render: async function ({ app, client, prisma }) {
 
-        const channels = await prisma.channel.findMany({
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
-        const c = channels.slice(0, 10).map(channel => `- ${channel.emoji} <#${channel.id}>${channel.personal? " :bust_in_silhouette:":""} (${channel.name})`).join("\n")
-        return `Here's a list of recently created channels:\n${c}`
-            .replaceAll("@", "​@")
-            .replaceAll(/[\u{1F3FB}-\u{1F3FF}]/gmu, "");
-    },
+    const channels = await prisma.channel.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+    const c = channels.slice(0, 10).map(channel => `- ${channel.emoji} <#${channel.id}>${channel.personal ? " :bust_in_silhouette:" : ""} ${channel.name ? `(${channel.name})` : ""}`).join("\n")
+    return `Here's a list of recently created channels:\n${c}`
+      .replaceAll("@", "​@")
+      .replaceAll(/[\u{1F3FB}-\u{1F3FF}]/gmu, "");
+  },
 };
